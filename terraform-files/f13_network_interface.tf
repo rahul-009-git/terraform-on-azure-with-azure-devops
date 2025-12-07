be073761d4e427caaf1a8b6b40307233347a5a87
Rahul Dhage <dhagerahul009@gmail.com>
2025-12-07 07:53:55 +0530

    new nic

--- terraform-files/f13_network_interface.tf
+++ terraform-files/f13_network_interface.tf
@@ -1,8 +1,13 @@
 
 resource "azurerm_network_interface" "vm_network_interface" {
-  name                = "${local.resource_name_prefix}-vm-nic"
+  name                = "${local.resource_name_prefix}-linux-vm-nic"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
+  
+  lifecycle {
+    create_before_destroy = true
+  }
+  
   ip_configuration {
     name                          = "linuxvm-ip-1"
     subnet_id                     = azurerm_subnet.web_subnet.id
