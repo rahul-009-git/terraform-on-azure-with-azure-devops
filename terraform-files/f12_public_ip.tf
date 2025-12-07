65251cb057827e0f360b7ec4ebf7a660c8886efe
Rahul Dhage <dhagerahul009@gmail.com>
2025-12-07 07:34:29 +0530

    recreate public ip for dns issue

--- terraform-files/f12_public_ip.tf
+++ terraform-files/f12_public_ip.tf
@@ -1,8 +1,10 @@
-resource "azurerm_public_ip" "vm_public_ip" {
-  name                = "${local.resource_name_prefix}-linuxvm-public-ip"
+resource "azurerm_public_ip" "linux_vm_public_ip" {
+  name                = "${local.resource_name_prefix}-linux-vm-public-ip"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   allocation_method   = "Static"
   sku                 = "Standard"
  // domain_name_label   = "app1-vm-label"
+  
+
 }
